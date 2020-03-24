ebc5f411ef5470d692cf8a6eb6c4453f49879c68
Tauriq_Barron <tauriqbarron@hotmail.co.nz>
2020-03-24 14:16:31 +1300

    Update test3.blade.php

--- resources/views/test3.blade.php
+++ resources/views/test3.blade.php
@@ -4,35 +4,35 @@
         <link rel="stylesheet" href="{{url('css/testcss.css')}}" type="text/css"/>
     </head>
     <body>
-    <div class="MainCon">
-        <div class="header">
-            <div class="headerCon">
-                <button class="login">Login</button>
-                <button class="login">Sign up</button>
+        <div class="MainCon">
+            <div class="header">
+                <div class="headerCon">
+                    <button class="login">Login</button>
+                    <button class="login">Sign up</button>
+                </div>
             </div>
-        </div>
-        <hr>
-        <div class="homeNav">
-            <div class="homeNavCon">
-                <ul class="navBar">
-                    <li class="topNav">Security</li>
-                    <li class="topNav">Property</li>
-                    <li class="topNav">News</li>
-                </ul>
+            <hr>
+            <div class="homeNav">
+                <div class="homeNavCon">
+                    <ul class="navBar">
+                        <li class="topNav">Security</li>
+                        <li class="topNav">Property</li>
+                        <li class="topNav">News</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <hr>
-        <div class="footer">
-            <div class="footerCon">
-                <ul class="footerLinks">
-                    <li>Contact Us</li>
-                    <li>About Us</li>
-                    <li>Terms and Conditions</li>
-                </ul>
+            <hr>
+            <div class="footer">
+                <div class="footerCon">
+                    <ul class="footerLinks">
+                        <li>Contact Us</li>
+                        <li>About Us</li>
+                        <li>Terms and Conditions</li>
+                    </ul>
+                </div>
             </div>
+            <p>Test</p>
         </div>
-        <p>Test</p>
-    </div>
 
     </body>
 </html>
